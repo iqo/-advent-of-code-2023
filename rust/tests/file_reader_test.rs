91777d78df943e975a4de4fb874dfe008e8f4683
Niklas Brynolf <niklas.brynolf@gmail.com>
2024-10-02 12:33:32 +0200

    utility

--- rust/tests/file_reader_test.rs
+++ rust/tests/file_reader_test.rs
@@ -1,14 +1,15 @@
 use std::io::{self, Write};
 use tempfile::NamedTempFile;
-use rust::read_file_lines;
+use rust::utility::read_file_lines;
 
 #[test]
-fn test_read_file_lines() -> io::Result<()> {
+fn test_read_file_lines() ->  io::Result<()> {
     let mut temp_file =  NamedTempFile::new()?;
 
     writeln!(temp_file, "Hello")?;
     writeln!(temp_file, "World")?;
     let file_path = temp_file.path().to_str().unwrap();
-    let line = read_file_lines(file_path)?;
-    assert_eq!(lines, vec!["Hello", "Woddrld"]);
+    let lines = read_file_lines(file_path)?;
+    assert_eq!(lines, vec!["Hello", "World"]);
+    Ok(())
 }
