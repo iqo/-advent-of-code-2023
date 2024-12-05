562b8ba2d934582d16e354b961cd9407437bfb2d
Niklas Brynolf <niklas.brynolf@gmail.com>
2024-12-05 17:52:02 +0100

    part 1 done

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,30 +1,25 @@
 use std::path::Path;
+use std::iter::zip;
 use utility::read_file::read_file_lines;
 
     fn main() -> std::io::Result<()> {
-        
-    /* 
-    3   4
-    4   3
-    2   5
-    1   3
-    3   9
-    3   3
-    */
-        let path = Path::new("day-01/data/").join("test_input.txt");
-        let lines = read_file_lines(path.to_str().unwrap())?;
-        total_distance(&lines);
+        let path = Path::new("day-01/data/").join("input.txt");
+        let file_input = read_file_lines(path.to_str().unwrap())?;
+        let result: i32 = total_distance(&file_input);
+        print!("Total distance: {}", result);
         Ok(())
     }
-    fn total_distance(input: &Vec<String>) {
-        let mut left_list: Vec<String> = Vec::new();
-        let mut rigth_list: Vec<String> = Vec::new();
+
+     fn total_distance(input: &Vec<String>) -> i32 {
+        let mut left_list: Vec<i32> = Vec::new();
+        let mut rigth_list: Vec<i32> = Vec::new();
         for line in input {
             let mut split_data = line.split_whitespace();
-            left_list.push(split_data.next().unwrap().parse::<i32>().unwrap().to_string());
-            rigth_list.push(split_data.next().unwrap().parse::<i32>().unwrap().to_string());
-            left_list.sort();
-            rigth_list.sort();
-        println!("left: {:?}, rigth: {:?}", left_list, rigth_list);
+            left_list.push(split_data.next().unwrap().parse::<i32>().unwrap());
+            rigth_list.push(split_data.next().unwrap().parse::<i32>().unwrap());
         }
-    }
+        left_list.sort();
+        rigth_list.sort();
+        let calcultade_distance: i32 = zip(left_list, rigth_list).map(|(a, b)|(a - b).abs()).sum();
+        calcultade_distance
+    } 
